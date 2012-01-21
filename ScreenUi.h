105f82f9bc4379591a17d1d59f5ba49eedf99bc2
Jason von Nieda <jason@vonnieda.org>
2012-01-20 17:28:18 -0800

    Changed license to LGPL 3

--- ScreenUi.h
+++ ScreenUi.h
@@ -1,11 +1,22 @@
 /**
+ * ScreenUi
  * A toolkit for building character based user interfaces on small displays.
- * 
  * Copyright (c) 2012 Jason von Nieda <jason@vonnieda.org>
- * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 
- * Unported License. To view a copy of this license, visit 
- * http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative 
- * Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ *
+ * This file is part of ScreenUi.
+ *
+ * ScreenUi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ScreenUi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ScreenUi.  If not, see <http://www.gnu.org/licenses/>. 
  */
 
 #ifndef __ScreenUi_h__
